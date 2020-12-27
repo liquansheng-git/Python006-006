f8baa1b7cd71fb70255b3fe280cbb2058e4a9907
liquansheng-git <603279429@qq.com>
2020-12-27 19:37:05 +0800

    提交作业002

--- week01/assignment.py
+++ week01/assignment.py
@@ -2,11 +2,10 @@ import logging
 import datetime
 import pathlib
 import os
+
 """
 编写一个函数, 当函数被调用时，将调用的时间记录在日志中, 日志文件的保存位置建议为：/var/log/python- 当前日期 /xxxx.log
 """
-
-
 # 1.写一个函数
 def func():
     # 获取当前的时间
@@ -20,23 +19,24 @@ def func():
 
 # 2.调用日志模块
 def studylogs():
-    p = pathlib.Path()
-    current_load = p.resolve()
+    current_load = pathlib.Path().resolve()
     current_date_log = datetime.datetime.today().strftime('%Y-%m-%d %H:%M:%S')
     current_date_file = datetime.datetime.today().strftime('%Y-%m-%d')
 # 3.把日志写到/var/log/python- 当前日期 /xxxx.log
     filedir = str(current_load) + "/var/log/"
-    filename =  str(current_load) + "/var/log/python-" + current_date_file + ".log"
+    filename = str(current_load) + "/var/log/python-" + current_date_file + ".log"
     # print(filename)
-    if os.path.exists(filename):
-        logging.basicConfig(filename=filename,
-                        level=logging.DEBUG,
-                        datefmt= "%Y-%m-%d %H:%M:%S",
-                        format="%(asctime)s %(name)-8s %(levelname)-8s [line:%(lineno)d] %(message)s")
-        logging.info("func函数在%s使用" % current_date_log)
-    else:
-        os.makedirs(filedir)
-        os.system(r"touch %s" % filename)
+    while True:
+        if os.path.exists(filename):
+            logging.basicConfig(filename=filename,
+                            level=logging.DEBUG,
+                            datefmt= "%Y-%m-%d %H:%M:%S",
+                            format="%(asctime)s %(name)-4s %(levelname)-4s [line:%(lineno)d] %(message)s")
+            logging.info("func()函数在%s使用" % current_date_log)
+            break
+        else:
+            os.makedirs(filedir)
+            os.system(r"touch %s" % filename)
 
 # 防止main函数其他模块调用，首先创建__main__出来
 if __name__ == "__main__":
